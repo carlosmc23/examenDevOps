0c20a93d67990fa2d80343a855bbefc39eb4d2bd
Carlos Meneses <kenshinmc23@gmail.com>
2018-08-03 22:10:46 -0400

    imprimiendo mundo en la rama develops

--- src/main/java/com/examen/devOps/HolaMundo.java
+++ src/main/java/com/examen/devOps/HolaMundo.java
@@ -3,5 +3,6 @@ package com.examen.devOps;
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("<<<<<<<<Hola>>>>>>>>");
+        System.out.println("<<<<<<<<Mundo>>>>>>>>");
     }
 }
