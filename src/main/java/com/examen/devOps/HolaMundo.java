8dd25939ef84f293ac4daa134855dba95ea7f9b6
Carlos Meneses <kenshinmc23@gmail.com>
2018-08-10 19:28:07 -0400

    implementando servicio REST hola mundo

--- src/main/java/com/examen/devOps/HolaMundo.java
+++ src/main/java/com/examen/devOps/HolaMundo.java
@@ -1,7 +1,13 @@
 package com.examen.devOps;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HolaMundo {
-    public static void main(String[] args) {
-        System.out.println("<<<<<<<<Hola>>>>>>>>");
+
+    @RequestMapping("/hola")
+    String hellow(){
+        return "hola mundo!!!!!";
     }
 }
---
+++ src/main/java/com/examen/devOps/PrintHolaMundo.java
@@ -0,0 +1,7 @@
+package com.examen.devOps;
+
+public class PrintHolaMundo {
+    public static void main(String[] args) {
+        System.out.println("<<<<<<<<Hola>>>>>>>>");
+    }
+}
